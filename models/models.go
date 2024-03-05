231dba853cde6871a7d2dd97a7a1081a673813dd
Enciso <dav_vd@hotmail.com>
2024-03-04 20:06:10 -0600

    fix productos

--- models/models.go
+++ models/models.go
@@ -1,6 +1,29 @@
 package models
 
+import "time"
+
 type ConfigInfo struct {
 	DBDriver string `mapstructure:"DB_DRIVER"`
 	DBSource string `mapstructure:"DB_SOURCE"`
+}
+
+type Product struct {
+	Id               string    `json:"id"`
+	Name             string    `json:"name"`
+	ShortDescription string    `json:"short_description"`
+	LargeDescription string    `json:"large_description"`
+	RegularPrice     float32   `json:"regular_price"`
+	SalesPrice       float32   `json:"sales_price"`
+	Color            string    `json:"color"`
+	Size             string    `json:"size"`
+	Stock            int       `json:"stock"`
+	Images           string    `json:"images"`
+	Code             string    `json:"code"`
+	SKU              string    `json:"sku"`
+	Category         string    `json:"category"`
+	Gender           string    `json:"gender"`
+	Published        bool      `json:"published"`
+	Active           bool      `json:"active"`
+	CreationDate     time.Time `json:"creation_date"`
+	UpdateDate       time.Time `json:"update_date"`
 }
